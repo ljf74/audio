64ac72f576cf3d9710e7f91b7a4d21b6f2dc551d
onworks14 <onworks14@gmail.com>
2023-12-30 07:05:51 +0000

    Add 明天你好_牛奶咖啡

--- song.js
+++ song.js
@@ -1,5 +1,6 @@
 const sounds = {
   party_少女时代:'https://db.ljf72.repl.co/audio/party_少女时代.mp3',
+  明天你好_牛奶咖啡:'https://db.ljf72.repl.co/audio/明天你好_牛奶咖啡.mp3',
   empty_winner:'https://db.ljf72.repl.co/audio/empty_winner.mp3',
   目及皆是你:'https://db.ljf72.repl.co/audio/目及皆是你.mp3',
   致爱_young_song:'https://db.ljf72.repl.co/audio/致爱_young_song.mp3',
