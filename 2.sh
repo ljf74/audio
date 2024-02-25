0c19c17649cad083e40817cba8e88c010a995c0e
onworks14 <150022695+onworks14@users.noreply.github.com>
2024-02-25 13:56:33 +0000

    Delete  some files or folders

--- 2.sh
+++
@@ -1,4 +0,0 @@
-#wget -O ./db/audio/可爱女人.mp3 https://sy-sycdn.kuwo.cn/7aed7c9c56c80296ae8cc15b5435894e/65c23cf6/resource/n3/63/76/3368440867.mp3
-# mv *png .jpg
-mv *jpg ./db/images/
-mv *mp3 ./db/audio/
