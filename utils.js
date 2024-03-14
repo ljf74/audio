b8d6097229eeed15be23b21d3d35bdea55751a49
onworks14 <150022695+onworks14@users.noreply.github.com>
2024-03-14 14:33:19 +0000

    Add backToTopBtn

--- utils.js
+++ utils.js
@@ -109,22 +109,23 @@ function getRandomColor() {
 }
 
 let calcScrollValue = () => {
-  let scrollProgress = document.getElementById('progress')
-  let progressValue = document.getElementById('progress-value')
-  let pos = document.documentElement.scrollTop
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
   let calcHeight =
     document.documentElement.scrollHeight -
-    document.documentElement.clientHeight
-  let scrollValue = Math.round((pos * 100) / calcHeight)
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
   if (pos > 100) {
-    scrollProgress.style.display = "grid"
+    scrollProgress.style.display = "grid";
   } else {
-    scrollProgress.style.display = "none"
+    scrollProgress.style.display = "none";
   }
-  scrollProgress.addEventListener('click', () => {
-    document.documentElement.scrollTop = 0
-  })
-  scrollProgress.style.background = `conic-gradient(#03ccc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`
-}
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
 
-window.onscroll = calcScrollValue
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
